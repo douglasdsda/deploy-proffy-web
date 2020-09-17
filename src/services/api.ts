c9be51ba7509c2aa287a6127a72e926cfc26bbd2
douglas.dev.2020 <douglas.dev.2020@gmail.com>
2020-09-17 06:17:30 -0300

    web

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://douglas-site-com-br.umbler.net',
-  // baseURL: 'https://api-proffy-douglas.herokuapp.com',
+  // baseURL: 'https://douglas-site-com-br.umbler.net',
+  baseURL: 'https://api-proffy-douglas.herokuapp.com',
   //  baseURL: 'http://localhost:3333',
 });
 
